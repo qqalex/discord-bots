9de941c632169a1fe5708794195d9d207627b37f
user <86507185+alexonmc@users.noreply.github.com>
2023-02-05 13:52:53 -0600

    Working build
    Still a work in progress.

---
+++ dog_bot.py
@@ -0,0 +1,41 @@
+import discord
+from discord import app_commands
+import time
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+guild_object = discord.Object(id=1067257535549157427)
+token = open('dog_bot_token.txt', 'r').read()
+
+watchdog_file = open('watchdog_file', 'a+')
+focus_file = open('focus_file', 'a+')
+
+@tree.command(name = "ping", description = "Poke the Shiba", guild=guild_object)
+async def ping_command(interaction):
+    message = f"\U0001f415 Sat in {int((time.time() - interaction.created_at.timestamp()) * 1000)} ms"
+    await interaction.response.send_message(message, ephemeral=True)
+
+@tree.command(name = "add", description = "Add username to droptime watchdog", guild=guild_object)
+async def add_command(interaction, username):
+    message = f'Added ``{username}`` to ``WatchDog``'
+    await interaction.response.send_message()
+
+@tree.command(name = "remove", description = "Remove username from watchdog", guild=guild_object)
+async def remove_command(interaction, username):
+    message = f'Removed ``{username}`` from ``WatchDog``'
+    await interaction.response.send_message()
+
+@tree.command(name = "focus", description= "A faster watchdog for just one account at a time", guild=guild_object)
+async def focus_command(interaction, username):
+    message = f'Added ``{username}`` to ``Focus``'
+    await interaction.response.send_message(message, ephemeral=True)
+
+@client.event
+async def on_ready():
+    await tree.sync(guild=guild_object)
+    await client.change_presence(status=discord.Status.online, activity=discord.Streaming(name='API requests.', platform='Twitch', url='https://www.twitch.tv/directory'))
+    print("Ready!")
+
+client.run(token)
---
+++ verification_bot.py
@@ -0,0 +1,57 @@
+import discord
+from discord import app_commands
+from captcha.image import ImageCaptcha
+import random
+import string
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+captchaImage = ImageCaptcha(fonts=['/home/ec2-user/fonts/Raleway.ttf', '/home/ec2-user/fonts/OpenSans.ttf'])
+token = open('verification_bot_token.txt', 'r').read()
+
+captchaList = {}
+
+def __generate_captcha(user_id):
+    captchaKey = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
+    captchaData = captchaImage.generate(captchaKey)
+    captchaImage.write(captchaKey, '/home/ec2-user/captcha.png')
+    captchaList[user_id] = captchaKey
+
+    return captchaData
+
+@tree.command(name="verify", description="Human verification", guild=discord.Object(id=1067257535549157427))
+async def verify_command(interaction):
+    captchaData = __generate_captcha(interaction.user.id)
+
+    captchaFile = discord.File(filename='/home/ec2-user/captcha.png', fp=captchaData, spoiler=False, description='Captcha Image')
+    await interaction.response.send_message('Do ``/solve <captcha>`` when you\'re done.', file=captchaFile, ephemeral=True, delete_after=600)
+
+
+@tree.command(name="solve", description="Submit a CAPTCHA answer", guild=discord.Object(id=1067257535549157427))
+async def solve_command(interaction, captcha_answer: str):
+    generatedAnswer = str(captchaList[interaction.user.id]).lower()
+    if generatedAnswer == captcha_answer.lower():
+        verified_role_id = 1069575551867682896
+        verified = discord.Object(verified_role_id)
+
+        velma = '<:velma:1071083663619535038>'
+
+        await interaction.user.add_roles(verified)
+        await interaction.response.send_message(f'You\'re Successfully Verified! {velma}', ephemeral=True, delete_after=600)
+    else:
+        await interaction.response.send_message('The CAPTCHA is wrong. Try again.', ephemeral=True, delete_after=600)
+
+@client.event
+async def on_message(message):
+    if message.channel.id == 1069595439202058280 and message.author.id != client.user.id and message.author.id != 381966093813678080:
+        
+
+
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=1067257535549157427))
+    await client.change_presence(status= discord.Status.online, activity= discord.Streaming(name='account verification.', platform='Twitch', url='https://www.twitch.tv/directory'))
+    print("Ready!")
+
+client.run(token)
